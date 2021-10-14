54163d5b59d9a7dc4cd5a87222f6f474a2d8d6f9
nascilino <nascilino@gmail.com>
2021-10-13 21:02:03 -0300

    main

--- index.js
+++ index.js
@@ -1,21 +1,27 @@
 const express = require("express");
 const path = require("path");
 const app = express();
+require("dotenv").config();
 const port = process.env.PORT || 3000;
 app.use(express.urlencoded());
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
 
-app.get("/", (req, res) => {
-    res.render("index");
-  });
-  app.get("/senha", (req, res) => {
-    res.render("senha");
-  });
-  app.get("/cadastro", (req, res) => {
-    res.render("cadastro")
-  
-  })
+const Filme = require("./models/filme");
 
-  app.listen(port, () =>
-  console.log(`Servidor rodando em http://localhost:${port}`))
+app.get("/", async (req, res) => {
+  const filmes = await Filme.findAll();
+  res.render("index", {
+    filmes,
+  });
+});
+app.get("/senha", (req, res) => {
+  res.render("senha");
+});
+app.get("/cadastro", (req, res) => {
+  res.render("cadastro");
+});
+
+app.listen(port, () =>
+  console.log(`Servidor rodando em http://localhost:${port}`)
+);
