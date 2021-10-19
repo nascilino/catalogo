c7b70ab5b66d6c03ec7e3f49a6a452999b68c15d
nascilino <nascilino@gmail.com>
2021-10-18 21:51:51 -0300

    main

--- database/index.js
+++ database/index.js
@@ -1,9 +1,12 @@
 const Sequelize = require("sequelize");
-const sequelize = new Sequelize(
-  process.env.DB_BASE,
-  process.env.DB_USER,
-  process.env.DB_PASS,
-  process.env.DB_PORT,
-  { host: process.env.DB_HOST, port: 5432, dialect: "postgres" }
-);
+const sequelize = new Sequelize(process.env.DATABASE_URL, {     
+  dialect: 'postgres',
+  protocol: 'postgres',     
+  dialectOptions: {         
+    ssl: {             
+      require: true,             
+      rejectUnauthorized: false         
+    }     
+  } 
+}) 
 module.exports = sequelize;
--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require("express");
+require("dotenv").config()
 const path = require("path");
 const app = express();
 require("dotenv").config();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 app.use(express.urlencoded());
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
